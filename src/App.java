f9a3e8fd2a2e29d3f7c18ce117bc14d29aa92b15
ivankossdev <ivan.koss@yandex.ru>
2024-08-21 13:39:25 +0300

    func

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Led!");
+        String str = "my string";
+        long lng = 15000555L;
+        System.out.printf("Example string %s \nLong intger %d\n", str, lng);
+        String res = func(2, 10);
+        System.out.printf("Func result %s\n", res);
+        String[] arrayString = new String[3];
+        setArrayString(arrayString);
+        getArrayString(arrayString);
+    }
+
+    static String func(int a, int b){
+        int r = (a + b) * 2;
+        String result = String.format("%d + %d = %d", a, b, r);
+        
+        return result;
+    }
+
+    static void setArrayString(String[] a){
+        for(int i = 0; i < 3; i++){
+            a[i] = String.format("string_%d", i);
+        }
+    }
+
+    static void getArrayString(String[] a){
+        for (String myString : a) {
+            System.out.println(myString);
+        }
     }
 }
